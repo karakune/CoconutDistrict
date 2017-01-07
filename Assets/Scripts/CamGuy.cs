3461d7bbf0099b222a627f51e9311986ff6fa679
karakune <hugopelletier783@gmail.com>
2017-01-07 15:06:33 -0500

    merging

--- Assets/Scripts/CamGuy.cs
+++ Assets/Scripts/CamGuy.cs
@@ -15,11 +15,11 @@ public class CamGuy : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-        Camera.main.transform.Translate(Input.GetAxis("Horizontal") * speed * Time.deltaTime,
-            Input.GetAxis("Vertical") * speed * Time.deltaTime * -1, 0);
+        Camera.main.transform.Translate(Input.GetAxis("Horizontal2") * speed * Time.deltaTime,
+            Input.GetAxis("Vertical2") * speed * Time.deltaTime * -1, 0);
         //Debug.Log(Input.GetAxis("Fire1"));
         //right trigger pressed
-        if (Input.GetAxis("Fire1") == -1 && !shotFired)
+        if (Input.GetAxis("Fire12") == -1 && !shotFired)
         {
             GameObject p1 = Instantiate(projectile);
             p1.transform.parent = Camera.main.transform;
---
+++ Assets/Scripts/Crosshair.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Crosshair : MonoBehaviour {
+
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    gameObject.transform.Translate(Input.GetAxis("Horizontal") * speed * Time.deltaTime,
+            Input.GetAxis("Vertical") * speed * Time.deltaTime * -1, 0);
+    }
+}
