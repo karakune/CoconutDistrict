34df6de23f9a32bc30c3ca08920edd0212632cd3
karakune <hugopelletier783@gmail.com>
2017-01-07 10:04:23 -0500

    controls for cam guy added, WIP: cam guy's projectiles

---
+++ Assets/Scripts/CamGuy.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class CamGuy : MonoBehaviour {
+
+    public float speed;
+    public GameObject projectile;
+    bool shotFired = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Camera.main.transform.Translate(Input.GetAxis("Horizontal") * speed * Time.deltaTime,
+            Input.GetAxis("Vertical") * speed * Time.deltaTime * -1, 0);
+        //Debug.Log(Input.GetAxis("Fire1"));
+        //right trigger pressed
+        if (Input.GetAxis("Fire1") == -1 && !shotFired)
+        {
+            GameObject p1 = Instantiate(projectile);
+            p1.transform.parent = Camera.main.transform;
+            shotFired = true;
+        }
+        //compensate for dead zone
+        //if(Math.Abs(Input.GetAxis("Fire1")) < 0.015)
+        //{
+        //    shotFired = false;
+        //}
+
+    }
+
+    public void SetShotFired(bool value)
+    {
+        shotFired = value;
+    }
+    //Input.GetKeyDown(KeyCode.JoystickButton1)
+    //Input.GetAxis("Horizontal")
+    //edit -> project settings -> Input -> Horizontal -> Type -> axis
+}
---
+++ Assets/Scripts/SlingshotProjectile.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SlingshotProjectile : MonoBehaviour {
+
+    public float ySpeed;
+    float proximity;
+    //number of frames before object is destroyed
+    public int lifespan;
+    int framesCounted = 0;
+
+    Camera parent;
+
+    Vector3 startSpeed;
+
+	// Use this for initialization
+	void Start () {
+        startSpeed = new Vector3(0, ySpeed, 1);
+        GetComponent<Rigidbody>().AddForce(startSpeed * Time.deltaTime);
+
+        parent = transform.parent.GetComponent<Camera>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        proximity = System.Math.Abs(0.1f / gameObject.transform.position.z);
+        //ScaleByProximity();
+
+        if(framesCounted < lifespan)
+        {
+            framesCounted++;
+        }else
+        {
+            parent.GetComponent<CamGuy>().SetShotFired(false);
+            Destroy(this.gameObject);
+        }
+    }
+
+    void ScaleByProximity()
+    {
+        transform.localScale = new Vector3(proximity, proximity, 1);
+    }
+}
