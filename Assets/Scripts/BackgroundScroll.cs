58008f7e781f6a229595642240cf2df5a5fdc9b7
karakune <hugopelletier783@gmail.com>
2017-01-08 14:37:50 -0500

    finished backgrounds and clouds

---
+++ Assets/Scripts/BackgroundScroll.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScroll : MonoBehaviour {
+
+    public float speed;
+    private float edgeDistanceX;
+    private float edgeDistanceY;
+    GameObject horEdge;
+    GameObject vertEdge;
+
+	// Use this for initialization
+	void Start () {
+        vertEdge = GameObject.Find("Top Edge");
+        horEdge = GameObject.Find("Right Edge");
+        edgeDistanceX = (gameObject.transform.localScale.x / 2) - 
+            (horEdge.transform.localPosition.x + horEdge.transform.localScale.x / 2);
+        edgeDistanceY = (gameObject.transform.localScale.y / 2) -
+            (horEdge.transform.localPosition.y + horEdge.transform.localScale.y / 2);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //stops the background from leaving the cam
+        if(transform.localPosition.x > edgeDistanceX)
+        {
+            transform.localPosition = new Vector3(edgeDistanceX,
+                transform.localPosition.y, transform.localPosition.z);
+        }
+        if (transform.localPosition.x < -edgeDistanceX)
+        {
+            transform.localPosition = new Vector3(-edgeDistanceX,
+                transform.localPosition.y, transform.localPosition.z);
+        }
+        if (transform.localPosition.y > edgeDistanceY)
+        {
+            transform.localPosition = new Vector3(transform.localPosition.x, edgeDistanceY,
+                transform.localPosition.z);
+        }
+        if (transform.localPosition.y < -edgeDistanceY)
+        {
+            transform.localPosition = new Vector3(transform.localPosition.x, -edgeDistanceY,
+                transform.localPosition.z);
+        }
+
+
+        gameObject.transform.Translate(Input.GetAxis("CamGuy X") * speed * Time.deltaTime * -1,
+            Input.GetAxis("CamGuy Y") * speed * Time.deltaTime, 0);
+    }
+}
---
+++ Assets/Scripts/CloudTile.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudTile : MonoBehaviour {
+    public float speed;
+    public float tileSizeX;
+    public float tileSizeY;
+
+    private Vector3 startPosition;
+    private float newPositionX;
+    private float newPositionY;
+
+	// Use this for initialization
+	void Start () {
+        startPosition = transform.position;
+        newPositionX = 0;
+        newPositionY = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Input.GetAxis("CamGuy X") * speed * Time.deltaTime, 0, 0);
+        newPositionX = Mathf.Repeat(newPositionX + Input.GetAxis("CamGuy X") * speed * Time.deltaTime, tileSizeX);
+        newPositionY = Mathf.Repeat(newPositionY + Input.GetAxis("CamGuy Y") * speed * Time.deltaTime, tileSizeY);
+        transform.localPosition = startPosition + (new Vector3(newPositionX, newPositionY, 0f));
+    }
+}
